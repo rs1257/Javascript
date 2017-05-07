2a9ddbc6e583b9b6edfaa7d99e956352779d2874
rs1257 <rs1257@york.ac.uk>
2017-05-07 10:03:31 +0100

    fixed functions. need to add bounds and add alert pop ups

--- UniGradeCalc/public_html/JS.js
+++ UniGradeCalc/public_html/JS.js
@@ -4,6 +4,10 @@ var rowcount = 2;
 window.setInterval(function(){
     gettotal();
     totalcredits();
+    truepercentage();
+    truetotal();
+    overalltotal();
+    grade();
 }, 2000);
 
 //table manipulation
@@ -30,7 +34,7 @@ function gettotal(){
         var total = ((+x[2].innerHTML * +x[3].innerHTML) + 
                 (+x[4].innerHTML * +x[5].innerHTML) + 
                 (+x[6].innerHTML * +x[7].innerHTML))* 0.01;
-        x[8].innerHTML = total;
+        x[8].innerHTML = total.toFixed(2);
     }
  }
  
@@ -45,45 +49,39 @@ function totalcredits(){
  }
  
 function truepercentage(){
-    var refTab = document.getElementById("tbl");
-    var ref = document.getElementById("tcred");
-    for ( var i = 1; row = refTab.rows[i]; i++ ) {
-        var row = refTab.rows[i];
-        var truepercentage = (100 / ref.rows[0].cells[3].innerHTML) * row.cells[1].innerHTML;
-        truepercentage = truepercentage.toFixed(2);
-        refTab.rows[i].cells[9].innerHTML = truepercentage;
+        var y = document.getElementById("tbl").rows[rowcount].cells;
+        for (var i = 0; i < rowcount-1; i++){
+            var x = document.getElementById("tbl").rows[rowcount-1].cells;
+            var truepercentage = (100 / y[3].innerHTML) * x[1].innerHTML;
+            x[9].innerHTML = truepercentage.toFixed(2);
     }
 }
 
-function truetotal(){
-    var refTab = document.getElementById("tbl");
-    for ( var i = 1; row = refTab.rows[i]; i++ ) {
-        var row = refTab.rows[i];
-        var truetotal = row.cells[8].innerHTML * (row.cells[9].innerHTML/100);
-        truetotal = truetotal.toFixed(2);
-        refTab.rows[i].cells[10].innerHTML = truetotal;
-    } 
+function truetotal(){ 
+    for (var i = 0; i < rowcount-1; i++){
+            var x = document.getElementById("tbl").rows[rowcount-1].cells;
+            var truetotal = x[8].innerHTML * (x[9].innerHTML / 100);
+            x[10].innerHTML = truetotal.toFixed(2);
+    }
 }
 
 function overalltotal(){
-    var refTab = document.getElementById("tbl");
     var overall = 0;
-    for ( var i = 1; row = refTab.rows[i]; i++ ) {
-        var row = refTab.rows[i];
-        overall = +overall + +row.cells[10].innerHTML;
-        var ref = document.getElementById("tbl").lastChild();
-        overall = overall.toFixed(2);
-        ref.rows[0].cells[5].innerHTML = +overall;
+    for (var i = 0; i < rowcount-1; i++){
+        var x = document.getElementById("tbl").rows[rowcount-1].cells;
+        overall += +x[10].innerHTML;
     }
+    var y = document.getElementById("tbl").rows[rowcount].cells;
+    y[5].innerHTML = overall.toFixed(2);
 }
 
-function grade(){
-    var ref = document.getElementById("tbl").lastChild();
-    var grade = ref.rows[0].cells[5].innerHTML;
-    if (grade < 40){ref.rows[0].cells[7].innerHTML = "Fail";} 
-    else if (grade < 50){ref.rows[0].cells[7].innerHTML = "3";}
-    else if (grade < 60){ref.rows[0].cells[7].innerHTML = "2/2";}
-    else if (grade < 70){ref.rows[0].cells[7].innerHTML = "2/1";}
-    else if (grade >= 70){ref.rows[0].cells[7].innerHTML = "1";}
-    else{ref.rows[0].cells[7].innerHTML = "ERROR";}
+function grade(){ //todo
+    var y = document.getElementById("tbl").rows[rowcount].cells;
+    var grade = y[5].innerHTML;
+    if (grade < 40){y[7].innerHTML = "Fail";} 
+    else if (grade < 50){y[7].innerHTML = "3";}
+    else if (grade < 60){y[7].innerHTML = "2/2";}
+    else if (grade < 70){y[7].innerHTML = "2/1";}
+    else if (grade >= 70){y[7].innerHTML = "1";}
+    else{y[7].innerHTML = "ERROR";}
 }
